e59f7eb3e31dc27cc84ed997f0d096b3f12cfa61
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-24 16:13:48 +0530

    function to get work Hrs

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,26 +5,29 @@ echo "Welcome to Employee Wage Computation Program"
 isFullTime=1
 isPartTime=2
 EMP_RATE_PER_HR=20
+MAX_HOURS_IN_MONTH=100
 MAX_WORKING_DAYS=20
 
 TOTAL_EMP_HRS=0
 TOTAL_WORKING_DAYS=0
-while [ $TOTAL_WORKING_DAYS -lt $MAX_WORKING_DAYS ]
+function getWorkingHrs() {
+	case $1 in
+		$isFullTime)
+			empHrs=8
+			;;
+		$isPartTime)
+			empHrs=4
+			;;
+		*)
+			empHrs=0
+			;;
+	esac
+}
+while [[ $TOTAL_EMP_HRS -lt $MAX_HOURS_IN_MONTH && $TOTAL_WORKING_DAYS -lt $MAX_WORKING_DAYS ]]
 do
-        empCheck=$((RANDOM%3));
-        case $empCheck in
-                $isFullTime)
-                        empHrs=8
-                        ;;
-                $isPartTime)
-                        empHrs=4
-                        ;;
-                *)
-                        empHrs=0
-                        ;;
-        esac
-        TOTAL_EMP_HRS=$(($TOTAL_EMP_HRS+$empHrs))
         ((TOTAL_WORKING_DAYS++))
+        getWorkingHrs $((RANDOM%3))
+        TOTAL_EMP_HRS=$(($TOTAL_EMP_HRS+$empHrs))
 done
 TOTAL_SALARY=$(($TOTAL_EMP_HRS*$EMP_RATE_PER_HR))
 echo "Total Salary :- " $TOTAL_SALARY
