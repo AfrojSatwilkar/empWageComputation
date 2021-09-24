285d8b8b7942380c17b3dd7fd37587ce4301e0c7
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-24 12:03:10 +0530

    Calculate wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,25 +3,30 @@
 echo "Welcome to Employee Wage Computation Program"
 
 
-
 isFullTime=1
 isPartTime=2
 empRatePerHr=20
-empCheck=$((RANDOM%3))
+maxWorkingDays=20
+totalEmpHrs=0
 
-case $empCheck in
-	$isFullTime)
-		empHrs=8
-		;;
-	$isPartTime)
-		empHrs=4
-		;;
-	*)
-		empHrs=0
-		;;
-esac
-empSalary=$(($empHrs*$empRatePerHr))
-echo "Total Salary : " $empSalary
-
-
+totalWorkingDays=0
+while [ $totalWorkingDays -lt $maxWorkingDays ]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3));
+        case $empCheck in
+                $isFullTime)
+                        empHrs=8
+                        ;;
+                $isPartTime)
+                        empHrs=4
+                        ;;
+                *)
+                        empHrs=0
+                        ;;
+        esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
+done
+totalEmpSalary=$(($totalEmpHrs*$empRatePerHr))
+echo "Total Salary :- " $totalEmpSalary
 
